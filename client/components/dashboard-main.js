554795ec8d65b67beaab99bead959d53f0aaf811
northPostal <north.postal@gmail.com>
2020-05-15 16:29:08 +0300

    edit root.js

---
+++ client/components/dashboard-main.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Route } from 'react-router-dom'  
+
+const Dashboard = () => {
+  return (
+    <div>
+      <div id="title"> Dashboard </div>
+      <Link to="/dashboard/profile/46a02278-f502-4416-a401-707cfef5cc86"> Go To Profile </Link>
+      <Link to="/dashboard/mail"> Go To Main </Link>
+        </div>
+  )
+}
+
+Dummy.propTypes = {}
+
+export default Dashboard
+
+// Внутри должно быть три элемента
+// // div с id="title" и текстом Main
+// ссылка(реакт) с навигацией /dashboard/profile/46a02278-f502-4416-a401-707cfef5cc86 и текстом "Go To Profile"
+// ссылка(реакт) с навигацией /dashboard и текстом "Go To Root"
